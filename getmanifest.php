cd73cd96f1277a8b4597c2f3e11473d297f36eff
OpenThread <openfibers@gmail.com>
2012-09-10 18:28:21 +0800

    test generate manifest.plist dynamically finished successfully

--- getmanifest.php
+++ getmanifest.php
@@ -6,17 +6,10 @@
 	require_once 'Classes/TOTClasses/HandleInfoPlistInPayload.php';
 	require_once 'Classes/TOTClasses/FileSystemHelper.php';
 	require_once 'Classes/TOTClasses/GenManifest.php';
+	require_once 'Classes/TOTClasses/XMLHelper.php';
 	require_once(__DIR__.'/Classes/ThirdPartyLib/CFPropertyList/CFPropertyList.php');
 
-	echo "<br/>";
-	echo "<pre>";
-	$manifestDictionary = GenManifest(
-		'http://192.168.1.129/issue//issue%201.0.ipa',
-	 	'com.openthread.issue',
-	 	'1.0 (1.0)',
-	 	'issue',
-	 	'Temp/manifest.plist');
-	var_dump($manifestDictionary);
-
-	echo "</pre>";
+	$xmlPath = "Documents/com.openthread.issue/1/manifest.plist";
+	$content = file_get_contents($xmlPath);
+	echo $content;
 ?>
