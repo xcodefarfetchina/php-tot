f872c723a1c317b807d1086831f7cce8fe0e9a0c
Canius Chu <canius.chu@farfetch.com>
2019-11-14 11:00:22 +0800

    helps

--- index.php
+++ index.php
@@ -39,6 +39,7 @@ function openPage(location)
 
 <!-- 顶部NavigationBar -->
 <div class="navigationBar">
+    <div style="position:absolute; top:11px; right:4px"><a href="Help/help.html" style="color: black;">Help</a></div>
 	<h1 class="headerTitle">Install Apps</h1>
 </div>
 
